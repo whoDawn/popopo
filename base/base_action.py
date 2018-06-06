2181103f0377a3dea0b2d920d6815ee5023bdf77
hitheima <hitheima@163.com>
2018-06-06 10:57:30 +0800

    first commit

---
+++ base/base_action.py
@@ -0,0 +1,23 @@
+from selenium.webdriver.support.wait import WebDriverWait
+
+
+class BaseAction:
+
+    def __init__(self, driver):
+        self.driver = driver
+
+    def click(self, loc):
+        self.find_element(loc).click()
+
+    def input_text(self, loc, text):
+        self.find_element(loc).send_keys(text)
+
+    def find_element(self, loc):
+        by = loc[0]
+        value = loc[1]
+        return WebDriverWait(self.driver, 5, 1).until(lambda x: x.find_element(by, value))
+
+    def find_elements(self, loc):
+        by = loc[0]
+        value = loc[1]
+        return WebDriverWait(self.driver, 5, 1).until(lambda x: x.find_elements(by, value))
---
+++ scripts/test_display.py
@@ -0,0 +1,34 @@
+import os, sys
+sys.path.append(os.getcwd())
+
+from appium import webdriver
+from base.base_driver import init_driver
+from page.display_page import DisplayPage
+
+class TestDisplay:
+
+    def setup(self):
+        self.driver = init_driver()
+        self.display_page = DisplayPage(self.driver)
+
+
+    def test_mobile_display_input(self):
+        # 点击放大镜
+        self.display_page.click_search()
+        # 输入文字
+        self.display_page.input_search_text("1")
+        # 点击返回
+        self.display_page.click_back()
+
+        # # 输入文字
+        # self.display_page.input_text("2")
+        # # 点击返回
+        # self.display_page.click_back()
+
+
+
+
+
+
+
+
